8de20ca521f2ba645a72dcb2b7f494eeb6a7aafa
Exerbaind <digr98@gmail.com>
2020-12-04 12:51:23 +0300

    fix

--- js/biblioteka.js
+++ js/biblioteka.js
@@ -1,15 +1,35 @@
-const sideArticlesHandler = document.querySelectorAll('.side-menu');
-const articlesBlock = document.querySelectorAll('.articles');
-const closeArticle = document.querySelectorAll('.articles__close_block');
+const sideArticlesHandler = document.querySelectorAll(".side-menu");
+const articlesBlock = document.querySelectorAll(".articles");
+const closeArticle = document.querySelectorAll(".articles__close_block");
 
 sideArticlesHandler.forEach((item, index) => {
-    item.addEventListener('click', function () {
-        articlesBlock[index].classList.toggle('show');
-    })
+  item.addEventListener("click", function () {
+    articlesBlock[index].classList.toggle("show");
+  });
 });
 
-// closeArticle.forEach((item, index) => {
-//     item.addEventListener('click', function () {
-//         articlesBlock[index].classList.remove('show');
-//     })
-// });
+// открытие и закрытие обзора youtube
+
+let openYouTubeReview = document.querySelector(".youtube-api");
+let collectionReview = document.querySelector(".collection-review");
+
+collectionReview.addEventListener("click", function () {
+  openYouTubeReview.classList.add("youtube-api__active");
+  document.body.classList.add("overflow-hidden");
+});
+
+openYouTubeReview.addEventListener("click", function (e) {
+  if (e.target == this) {
+    openYouTubeReview.classList.remove("youtube-api__active");
+    document.body.classList.remove("overflow-hidden");
+    player.stopVideo();
+  }
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.code == "Escape") {
+    openYouTubeReview.classList.remove("youtube-api__active");
+    document.body.classList.remove("overflow-hidden");
+    player.stopVideo();
+  }
+});
